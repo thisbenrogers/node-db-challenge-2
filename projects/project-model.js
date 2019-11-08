895be821139c19efd24456155422a088010c715b
Ben <thisbenrogers@gmail.com>
2019-11-08 13:02:09 -0600

    :construction: Begins Task endpoints
    Stops for end of Sprint Challenge

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,9 +1,11 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  addProject,
+  getProjects,
   getProjectById,
-  getProjects
+  findTasks,
+  addProject,
+  addTask
 }
 
 function getProjects() {
@@ -16,7 +18,24 @@ function getProjectById(id) {
   .first();
 }
 
+function findTasks(id) {
+  return db("tasks as t")
+    .join("projects as p", "p.id", "t.pro_id" )
+    .select("p.pro_name as projectName", "p.pro_desc as projectDescription", "t.id as id", "t.task_desc as taskDescription", "t.notes", "t.completed")
+    .where({ pro_id: id })
+    .orderBy("id")
+}
+
 async function addProject(proObj) {
   const [id] = await db('projects').insert(proObj, 'id');
   return getProjectById(id);
+}
+
+async function addTask(task, project_id) {
+  console.log("task in addTask:", task)
+  const [id] = await db('projects')
+    .insert({ ...task, project_id }, 'id')
+    .returning('*');
+    console.log("id in await:", id);
+  return findTasks(id);
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -14,6 +14,38 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Projects.getProjectById(id)
+  .then(project => {
+    if (project) {
+      res.json(project);
+    } else {
+      res.status(404).json({ message: 'Could not find project with given id.' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get projects' });
+  });
+});
+
+router.get('/:id/tasks', (req, res) => {
+  const { id } = req.params;
+
+  Projects.findTasks(id)
+  .then(tasks => {
+    if (tasks.length) {
+      res.json(tasks);
+    } else {
+      res.status(404).json({ message: 'Could not find tasks for given project' })
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to get tasks' });
+  });
+});
+
 router.post('/', (req, res) => {
   const projectData = req.body;
 
@@ -23,6 +55,28 @@ router.post('/', (req, res) => {
   })
   .catch (err => {
     res.status(500).json({ message: 'Failed to create new project' });
+  });
+});
+
+router.post('/:id/tasks', (req, res) => {
+  const taskData = req.body;
+  const { id } = req.params; 
+
+  // console.log("taskData in post:", taskData);
+
+  Projects.getProjectById(id)
+  .then(project => {
+    if (project) {
+      Projects.addTask(taskData, id)
+      .then(task => {
+        res.status(201).json(task);
+      })
+    } else {
+      res.status(404).json({ message: 'Could not find project with given id.' })
+    }
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to create new task' });
   });
 });
 
