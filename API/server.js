ad60efa50faefbc95f7aec0ca8342e2b41884e2f
Ben <thisbenrogers@gmail.com>
2019-11-08 12:38:54 -0600

    :sparkles: Adds logger middleware

--- API/server.js
+++ API/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-// const logger = require('../middleware/logger');
+const logger = require('../middleware/logger');
 
 const projectRouter = require("../projects/project-router.js");
 const resourceRouter = require('../resources/resource-router');
@@ -10,7 +10,7 @@ const resourceRouter = require('../resources/resource-router');
 const server = express();
 
 server.use(helmet());
-// server.use(logger);
+server.use(logger);
 server.use(express.json());
 server.use(cors());
 
