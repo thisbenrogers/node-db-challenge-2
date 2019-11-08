aeeef530d91fd0b0271740a469fa724a7e917f4a
Ben <thisbenrogers@gmail.com>
2019-11-08 10:43:42 -0600

    :card_file_box: Migrates four tables in new db
    tables: projects, resources, tasks, pro_res

---
+++ data/migrations/20191108102410_projects.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+
+  return knex.schema.createTable('projects', project => {
+
+    project.increments();
+
+    project.string('pro_name', 255)
+      .notNullable();
+
+    project.string('pro_desc', 1024);
+
+    project.boolean('completed')
+      .notNullable()
+      .defaultTo(false);
+
+  })
+  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('projects');
+};
---
+++ data/migrations/20191108103539_tasks.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('tasks', task => {
+
+    task.increments();
+
+    task.string('task_desc', 255)
+      .notNullable();
+
+    task.string('notes', 1024);
+
+    task.boolean('completed')
+      .notNullable()
+      .defaultsTo(false);
+
+    task.integer('pro_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('tasks');
+};
